bda1d89a6a1d458ca71d45735293d102db95420b
Shaver <bshaver09@bshaver09m4.local>
2014-02-21 11:19:11 -0600

    Initial Commit

---
+++ SlidingPuzzle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlidingPuzzle
+//
+//  Created by Shaver, Brandon L on 2/21/14.
+//  Copyright (c) 2014 Shaver, Brandon L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
